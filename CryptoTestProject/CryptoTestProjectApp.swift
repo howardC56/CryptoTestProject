cf6df74839fbdea18202adc07321a7ddb3db2b72
howardC56 <h2chang56@gmail.com>
2025-07-07 16:28:53 -0400

    Initial Commit

---
+++ CryptoTestProject/CryptoTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTestProjectApp.swift
+//  CryptoTestProject
+//
+//  Created by Howard Chang on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoTestProjectTests/CryptoTestProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTestProjectTests.swift
+//  CryptoTestProjectTests
+//
+//  Created by Howard Chang on 7/7/25.
+//
+
+import Testing
+@testable import CryptoTestProject
+
+struct CryptoTestProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
